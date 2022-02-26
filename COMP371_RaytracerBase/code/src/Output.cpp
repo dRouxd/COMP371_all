477da1d02d50330067272c2e6a40c06bee511d22
danny roux <danny.roux@hotmail.fr>
2022-02-25 22:54:07 -0500

    Added output optional members

--- COMP371_RaytracerBase/code/src/Output.cpp
+++ COMP371_RaytracerBase/code/src/Output.cpp
@@ -10,9 +10,52 @@ Output::Output(nlohmann::json j)
     
     this->ai = JsonArrayToVector3f(j["ai"]);
     this->bkc = JsonArrayToVector3f(j["bkc"]);
+
+    this->raysPerPixel = NULL;
+    this->raysPerPixelSize = 0;
+    if(j.contains("raysperpixel"))
+    {
+        this->raysPerPixelSize = j["raysperpixel"].size();
+        if(this->raysPerPixelSize > 0)
+        {
+            this->raysPerPixel = new unsigned int[this->raysPerPixelSize];
+            
+            int i = 0;
+            for(auto it = j["raysperpixel"].begin(); it != j["raysperpixel"].end(); ++it, ++i)
+            {
+                this->raysPerPixel[i] = (*it).get<unsigned int>();
+            }
+        }            
+    }
+
+    this->speedup = 0;
+    if(j.contains("speedup"))
+    {
+        this->speedup = j["speedup"].get<unsigned int>();
+    }
+    
+    this->antialiasing = false;
+    if(j.contains("antialiasing"))
+    {
+        this->antialiasing = j["antialiasing"].get<bool>();
+    }
+
+    this->twosiderender = true;
+    if(j.contains("twosiderender"))
+    {
+        this->twosiderender = j["twosiderender"].get<bool>();
+    }
+
+
+    if(j.contains("globalillum"))
+    {
+        this->globalillum = j["globalillum"].get<bool>();
+    }
+    this->globalillum = false;
 }
 
 Output::~Output()
 {
     delete this->camera;
+    delete this->raysPerPixel;
 }
--- COMP371_RaytracerBase/code/src/Output.h
+++ COMP371_RaytracerBase/code/src/Output.h
@@ -17,12 +17,27 @@ public:
     inline Camera* getCamera() const { return camera; };
     inline Eigen::Vector3f getAI() const { return ai; };
     inline Eigen::Vector3f getBKC() const { return bkc; };
+    inline unsigned int* getRaysPerPixel() const { return raysPerPixel; };
+    inline unsigned int getRaysPerPixelSize() const { return raysPerPixelSize; };
+    inline unsigned int getSpeedup() const { return speedup; };
+    inline bool getAntialias() const { return antialiasing; };
+    inline bool getTwoSiseRender() const { return twosiderender; };
+    inline bool getGlobalIllum() const { return globalillum; };
 
 private:
     std::string outputFilename;
     Camera* camera;
     Eigen::Vector3f ai;
     Eigen::Vector3f bkc;
+
+    unsigned int* raysPerPixel;
+    unsigned int raysPerPixelSize;
+
+    unsigned int speedup;
+
+    bool antialiasing;
+    bool twosiderender;
+    bool globalillum;
 };
 
 #endif
